c5cec4b08310b03beab60a3223bddfc36520c9fe
Mykola Hohsdze <Mykola_Hohsadze@epam.com>
2022-10-17 00:08:12 +0300

    Initial commit

---
+++ include/key_value_pair.h
@@ -0,0 +1,9 @@
+#ifndef BIGDATALAB1_KEY_VALUE_PAIR_H
+#define BIGDATALAB1_KEY_VALUE_PAIR_H
+
+// Definitions of key_value_pair
+#define KEY_VALUE_PAIR_KEY      0
+#define KEY_VALUE_PAIR_VALUE    8
+#define KEY_VALUE_PAIR_SIZE     16
+
+#endif //BIGDATALAB1_KEY_VALUE_PAIR_H
---
+++ include/linked_list.h
@@ -0,0 +1,15 @@
+#ifndef BIGDATALAB1_LINKED_LIST_H
+#define BIGDATALAB1_LINKED_LIST_H
+
+// Definitions of linked_list
+#define LINKED_LIST_HEAD  0
+#define LINKED_LIST_TAIL  8
+#define LINKED_LIST_COUNT 16
+#define LINKED_LIST_SIZE  24
+
+// Definitions of node
+#define NODE_VALUE  0
+#define NODE_NEXT   8
+#define NODE_SIZE   16
+
+#endif //BIGDATALAB1_LINKED_LIST_H
